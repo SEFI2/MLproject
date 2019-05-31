f2665ff45e0dbc66dde0de7d2b6e33c6a284b8e2
sefi2 <sefi2@unist.ac.kr>
2019-06-01 05:56:09 +0900

    refactor code

--- code.py
+++ code.py
@@ -3,31 +3,27 @@ from matplotlib import pyplot as plt
 import sys
 import argparse
 
-def run(args):
+DEFAULT_IMAGE_PATH = 'car1.jpg'
+
+def find_cars(args):
     car_cascade = cv2.CascadeClassifier('cars.xml')
     img = cv2.imread(args.image_path, 1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Detect cars
     cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    return cars, img
 
-    ncars = 0
 
-    # Draw border
+# Draws frames for the cars
+def show_frames(cars, img): 
     for (x, y, w, h) in cars:
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
-        print ("CAR detected:")
-        print (x, y, w, h)
-        
-        ncars = ncars + 1
-
-
-    print ("Number of cars in the image: ", ncars)
     plt.figure(figsize=(10,20))
     plt.imshow(img)
 
-
-DEFAULT_IMAGE_PATH = 'car1.jpg'
+# Show positions of each car in image
+def show_pos(cars): 
+    for (x, y, w, h) in cars:
+        print ("CAR detected: X={0} Y={1} W={2} H={3}".format(x, y, w, h))
 
 def get_parser(args):
     parser = argparse.ArgumentParser()
@@ -37,4 +33,8 @@ def get_parser(args):
 
 if __name__ == '__main__':
     args = get_parser(sys.argv[1:])
-    run(args)
+    cars, img = find_cars(args)
+    show_frames(cars, img)
+    show_pos(cars)
+    print ("Cars detected: {0}".format(len(cars)))
+    
